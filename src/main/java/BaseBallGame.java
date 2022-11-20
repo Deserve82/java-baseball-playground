1558a38880d5416e8c9c5e28df9bba7cc72f727e
deserve82 <inth8204@gmail.com>
2022-11-20 18:57:48 +0900

    feat : 게임 종료 후 다시 시작하는 것과 종료 중 선택하여 게임 실행

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -1,6 +1,60 @@
+import domain.ball.BallResult;
+import domain.ball.Balls;
+import domain.generator.RandomNumberGenerator;
+import ui.InputView;
+import ui.OutputView;
+
 public class BaseBallGame {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    private static Balls computer;
+    private static Balls user;
+
 
     public static void main(String[] args) {
 
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(9);
+
+        user = getNumberFromUser();
+        computer = randomNumberGenerator.generate();
+
+        BallResult result = compareAndPrintResult();
+
+        boolean stop = false;
+        while (!stop) {
+            user = getNumberFromUser();
+            result = compareAndPrintResult();
+            stop = checkGameContinueOrNot(result);
+        }
+    }
+
+    private static boolean checkGameContinueOrNot(BallResult result) {
+        if (result.isEnd()) {
+            outputView.endGame();
+            int value = inputView.inputGameOverOrContinue();
+            return restartOrNot(value);
+        }
+        return false;
+    }
+
+    private static BallResult compareAndPrintResult() {
+        BallResult result = computer.compare(user);
+        outputView.printBallCount(result);
+        return result;
+    }
+
+    private static Balls getNumberFromUser() {
+        outputView.printTypeNumber();
+        return new Balls(inputView.inputNumber());
+    }
+
+    private static boolean restartOrNot(int value) {
+        if (value == 1) {
+            computer = new RandomNumberGenerator(9).generate();
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -1,13 +1,17 @@
 package ui;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     Scanner sc = new Scanner(System.in);
 
-    public String inputNumber() {
-        return sc.next();
+    public List<Integer> inputNumber() {
+        return Arrays.stream(sc.next().split("")).map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     public int inputGameOverOrContinue() {
